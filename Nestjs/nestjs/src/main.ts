951fbe24a088c0313c766bcaa0a0e873baa88368
sh95fit <sh95fit@gmail.com>
2025-06-05 17:48:18 +0900

    Nestjs 초기 프로젝트 구성 및 메커니즘 분석

---
+++ Nestjs/nestjs/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API 문서입니다')
+    .setVersion('1.0')
+    .addTag('example')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // http://localhost:3000/api
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
