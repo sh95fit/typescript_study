b04fa798708ab6e06d4e10825680adffee0f4351
h95fit <h95fit@gmail.com>
2025-06-09 23:25:38 +0900

    Nest.js - controller 작성 기초

--- Nestjs/nestjs/src/users/users.controller.ts
+++ Nestjs/nestjs/src/users/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Body, Post, Put, Delete } from '@nestjs/common';
+import { CreateUserDto } from './CreateUserDto';
+import { UpdateUserDto } from './UpdateUserDto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+    findAll(): string {
+        return 'This action returns all users';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): string {
+        return `This action returns a #${id} user`;
+    }
+
+    @Post()
+    create(@Body() createUserDto: CreateUserDto) {
+        return 'This action adds a new user';
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        return `This action updates a #${id} user`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes a #${id} user`;
+    }
+}
